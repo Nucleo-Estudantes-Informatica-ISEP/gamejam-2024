476d768a1758c1775baf1c4bb8d8397d38efd317
henriquelspinto <1211201@isep.ipp.pt>
2024-05-12 21:41:11 +0100

    feat: judges container

---
+++ src/components/Judges/JudgeItem.tsx
@@ -0,0 +1,78 @@
+import { motion } from 'framer-motion';
+import { useState } from 'react';
+import useIsMobile from '../../hooks/useIsMobile';
+
+interface JudgeItemProps {
+  name: string;
+  image: string;
+  description: string;
+  isLeft: boolean;
+  overDescription: string;
+}
+
+const JudgeItem: React.FC<JudgeItemProps> = ({
+                                               name,
+                                               image,
+                                               description,
+                                               isLeft,
+                                               overDescription
+                                             }: JudgeItemProps) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const isMobile = useIsMobile();
+
+  return (
+    <article className="relative w-full flex items-center justify-center">
+      <motion.div
+        onMouseOver={() => {
+          setIsHovered(true);
+        }
+      }
+        onMouseDown={
+          () => {
+            if (isMobile)
+              setIsHovered((cur) => (cur === true ? false : true))
+          }
+        }
+        onMouseLeave={() => {
+          setIsHovered(false);
+        }}
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        animate={{
+          scale: isHovered ? 1.05 : 1
+        }}
+        transition={{ opacity: { duration: 0.5 }, duration: 0.2 }}
+        viewport={{ once: true }}
+        className={`bg-background-light max-w-xs w-full rounded-lg px-4 py-6 cursor-pointer z-0 duration-100 ease-in-out 
+        ${isHovered && 'shadow-intense-shadow shadow-green-600'}`}>
+        <div className="flex flex-col items-start min-h-fit h-[24rem] gap-6">
+          <img
+            className="rounded-md w-full aspect-[23/25] justify-center items-center mx-auto "
+            src={image}
+            alt={name}
+          />
+          <div className="flex flex-col justify-between">
+            <h3 className="text-xl md:text-2xl">{name}</h3>
+            <p className="text-sm md:text-md">{description}</p>
+          </div>
+        </div>
+      </motion.div>
+      <motion.p
+        onMouseOver={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+        onMouseDown={() => setIsHovered(false)}
+        className={`absolute w-full ${
+          isLeft ? 'md:-inset-x-1/2' : 'md:inset-x-1/2'
+        } bottom-72 bg-white border-slate-400 border-2 rounded-lg text-black text-sm text-center px-4 py-2 z-10`}
+        animate={{
+          scale: isHovered ? [0, 1] : 0,
+          opacity: isHovered ? 1 : 0
+        }}
+        transition={{ duration: 0.4 }}>
+        {overDescription}
+      </motion.p>
+    </article>
+  );
+};
+
+export default JudgeItem;
---
+++ src/components/Judges/Judges.tsx
@@ -0,0 +1,40 @@
+import JudgeItem from './JudgeItem';
+
+const Judges: React.FC = () => {
+
+  return (
+    <section
+      className="w-full items-center justify-around md:justify-around gap-y-24 md:gap-x-6 flex flex-col md:flex-row z-0">
+      <JudgeItem
+        name="Carlos Vaz de Carvalho"
+        description="Professor Coordenador do DEI"
+        image="/judges/carlos.jpg"
+        isLeft={false}
+        overDescription="Professor Coordenador do DEI no Instituto Superior de Engenharia do Porto (ISEP). Diretor da Virtual Campus Lda, uma PME (pequena média empresa) dedicada à Aprendizagem Aprimorada por Tecnologia e “Serious Games”"
+      />
+      <JudgeItem
+        name="Dulce Mota"
+        description="Professora e sub-diretora do DEI"
+        image="/judges/dulce.jpg"
+        isLeft={false}
+        overDescription="Doutorada em Engenharia Informática pela FEUP, é Professora no DEI do ISEP desde 1992. Subdiretora do DEI desde 2020. Suas áreas de pesquisa incluem Sistemas de Recomendação e Ensino à Distância."
+      />
+      <JudgeItem
+        name="Marílio Cardoso"
+        description="Professor Adjunto e sub-diretor do DEI"
+        image="/judges/marilio.jpeg"
+        isLeft={true}
+        overDescription="Licenciado em Engenharia Eletrotécnica. Experiência em eficiência energética e sistemas educativos inovadores. Principais interesses: Aprendizado de programação, sistemas educacionais inovadores e soluções centradas no ensino de ciência da computação e multimedia."
+      />
+      <JudgeItem
+        name="Gonçalo Pinto"
+        description="Streamer na Twitch"
+        image="/judges/gonçalo_pinto.jpeg"
+        isLeft={true}
+        overDescription="O meu nome é Gonçalo, tenho 19 anos. Desde o dia 24/01 que estou full-time streamer pois suspendi a matrícula da faculdade para lutar pelo meu sonho de criança, tendo começado a streamar diariamente desde 24/07."
+      />
+    </section>
+  );
+};
+
+export default Judges;
